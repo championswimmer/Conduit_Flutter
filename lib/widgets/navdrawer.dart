68f8bee4f1ea6763a018897fee5d1c6bc1765e06
Arnav Gupta <arnav@codingblocks.com>
2019-02-12 04:30:55 +0530

    drawre ui updates
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- lib/widgets/navdrawer.dart
+++ lib/widgets/navdrawer.dart
@@ -5,16 +5,30 @@ class MainNavDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           FlatButton(
-            child: Text('Home'),
+            child: Row(
+              children: <Widget>[
+                Icon(Icons.home),
+                Padding(padding: EdgeInsets.all(10),),
+                Text('Home')
+              ],
+            ),
             onPressed: () {
               Navigator.pushReplacementNamed(context, '/');
             },
           ),
           FlatButton(
-            child: Text('Settings'),
+            child: Row(
+              children: <Widget>[
+                Icon(Icons.settings),
+                Padding(padding: EdgeInsets.all(10),),
+                Text('Settings')
+              ],
+            ),
             onPressed: () {
               Navigator.pushReplacementNamed(context, '/settings');
             },
