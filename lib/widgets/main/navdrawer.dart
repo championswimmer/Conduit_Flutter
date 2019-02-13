e0cfd97ff482ac6a0729d60d33b5a0282b444888
Arnav Gupta <arnav@codingblocks.com>
2019-02-14 02:06:33 +0530

    navdrawer styling done
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- lib/widgets/main/navdrawer.dart
+++ lib/widgets/main/navdrawer.dart
@@ -1,40 +1,51 @@
-import 'package:conduit/main.dart';
 import 'package:flutter/material.dart';
 
 class MainNavDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          FlatButton(
-            child: Row(
-              children: <Widget>[
-                Icon(Icons.home),
-                Padding(padding: EdgeInsets.all(10),),
-                Text('Home')
-              ],
-            ),
-            onPressed: () {
-              Navigator.pushReplacementNamed(context, '/');
-            },
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.symmetric(vertical: 50),
+          color: Colors.green[500],
+          alignment: Alignment(0, 0),
+          child: Text(
+            'conduit',
+            style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
           ),
-          FlatButton(
-            child: Row(
-              children: <Widget>[
-                Icon(Icons.settings),
-                Padding(padding: EdgeInsets.all(10),),
-                Text('Settings')
-              ],
-            ),
-            onPressed: () {
-              Navigator.pushReplacementNamed(context, '/settings');
-            },
-          )
-        ],
-      )
-    );
+        ),
+        FlatButton(
+          child: Row(
+            children: <Widget>[
+              Icon(Icons.home),
+              Padding(
+                padding: EdgeInsets.all(10),
+              ),
+              Text('Home')
+            ],
+          ),
+          onPressed: () {
+            Navigator.pushReplacementNamed(context, '/');
+          },
+        ),
+        FlatButton(
+          child: Row(
+            children: <Widget>[
+              Icon(Icons.settings),
+              Padding(
+                padding: EdgeInsets.all(10),
+              ),
+              Text('Settings')
+            ],
+          ),
+          onPressed: () {
+            Navigator.pushReplacementNamed(context, '/settings');
+          },
+        )
+      ],
+    ));
   }
 }
